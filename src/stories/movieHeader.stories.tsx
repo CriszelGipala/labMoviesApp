a7eb75234abb0c0a232dd90e8e26ef6c08167a8a
Criszel Gipala <cznoinska@gmail.com>
2026-07-04 13:11:37 +0100

    Fix broken stories in Storybook.

--- src/stories/movieHeader.stories.tsx
+++ src/stories/movieHeader.stories.tsx
@@ -1,15 +1,21 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import MovieHeader from "../components/headerMovieList";
 import SampleMovie from "./sampleData";
+import { MemoryRouter } from "react-router";
 
 const meta = {
     title: "Movie Details Page/MovieHeader",
     component: MovieHeader,
+    decorators: [
+        (Story) => <MemoryRouter initialEntries={["/"]}>{Story()}</MemoryRouter>,
+    ],
 } satisfies Meta<typeof MovieHeader>;
 export default meta;
 
 type Story = StoryObj<typeof meta>;
 export const Basic: Story = {
-    args: SampleMovie
+    args: {
+        ...SampleMovie
+    }
 };
 Basic.storyName = "Default";
--- src/stories/movieListHeader.stories.tsx
+++ src/stories/movieListHeader.stories.tsx
@@ -1,9 +1,15 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import MovieListHeader from "../components/headerMovieList";
+import { MemoryRouter } from "react-router";
+import MoviesContextProvider from "../contexts/moviesContext";
 
 const meta = {
     title: 'Home Page/Header',
     component: MovieListHeader,
+    decorators: [
+      (Story) => <MemoryRouter initialEntries={["/"]}>{Story()}</MemoryRouter>,
+      (Story) => <MoviesContextProvider>{Story()}</MoviesContextProvider>,
+    ],
   } satisfies Meta<typeof MovieListHeader>;
   
   export default meta;
@@ -12,6 +18,7 @@ const meta = {
 
 export const Basic: Story = {
   args:{ title:'Discover Movies'}
-};
 
+};
 Basic.storyName = "Default";
+
