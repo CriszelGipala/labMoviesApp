be82dffd5ca4cbc9843396c657eb64e65b2be367
Criszel Gipala <cznoinska@gmail.com>
2026-07-04 13:36:37 +0100

    New button for Upcoming movies page.

---
+++ src/components/cardIcons/addToPlaylist.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import PlaylistAddIcon from "@mui/icons-material/PlaylistAdd";
+import { BaseMovieProps } from "../../types/interfaces";
+
+const AddToPlaylistIcon: React.FC<BaseMovieProps> = (movie) => {
+  void movie;
+  return <PlaylistAddIcon color="primary" fontSize="large" />;
+};
+
+export default AddToPlaylistIcon;
--- src/pages/upcomingMoviesPage.tsx
+++ src/pages/upcomingMoviesPage.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import PageTemplate from '../components/templateMovieListPage';
 import { BaseMovieProps } from "../types/interfaces";
 import { getUpcomingMovies } from "../api/tmdb-api";
-import AddToFavouritesIcon from '../components/cardIcons/addToFavourites'
+import AddToPlaylistIcon from '../components/cardIcons/addToPlaylist'
 
 const UpcomingMoviesPage: React.FC = () => {
   const [movies, setMovies] = useState<BaseMovieProps[]>([]);
@@ -19,7 +19,7 @@ const UpcomingMoviesPage: React.FC = () => {
       title='Upcoming Movies'
       movies={movies}
       action={(movie: BaseMovieProps) => {
-        return <AddToFavouritesIcon {...movie} />
+        return <AddToPlaylistIcon {...movie} />
       }}
     />
   );
