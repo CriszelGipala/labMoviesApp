77ad7b8adcb83af6bc9fa6a5119cce147270174d
Criszel Gipala <cznoinska@gmail.com>
2026-08-23 13:11:38 +0100

    Connect popular actors page.

--- src/index.tsx
+++ src/index.tsx
@@ -9,6 +9,7 @@ import FavouriteMoviesPage from "./pages/favouriteMoviesPage"; // NEW
 import MovieReviewPage from "./pages/movieReviewPage";
 import UpcomingMoviesPage from "./pages/upcomingMoviesPage";
 import AddMovieReviewPage from './pages/addMovieReviewPage';
+import PopularPeoplePage from "./pages/popularPeoplePage";
 import SiteHeader from './components/siteHeader'
 import MoviesContextProvider from "./contexts/moviesContext";
 
@@ -33,6 +34,7 @@ const App = () => {
             <Route path="/reviews/:id" element={<MovieReviewPage/>} />
             <Route path="/movies/favourites" element={<FavouriteMoviesPage />} />
             <Route path="/movies/upcoming" element={<UpcomingMoviesPage />} />
+            <Route path="/people" element={<PopularPeoplePage />} />
             <Route path="/movies/:id" element={<MoviePage />} />
             <Route path="/" element={<HomePage />} />
             <Route path="*" element={<Navigate to="/" />} />
